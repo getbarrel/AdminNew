fffed8a1a88d348b5b093b17692768bf298245bc
LIMWOOCHUL <kmstar72@naver.com>
2024-05-03 14:22:11 +0900

    24.05.03 상품 이미지내 마크 설정

--- www/admin/design/product_icon.act.php
+++ www/admin/design/product_icon.act.php
@@ -12,8 +12,14 @@ if($act == "insert"){
 	if(!is_dir($path)){
 		mkdir($path, 0777);
 	}
+	//PC Icon 
 	if ($_FILES["icon_file"]["size"] > 0){
 		move_uploaded_file($_FILES["icon_file"]["tmp_name"], $_SERVER["DOCUMENT_ROOT"]."".$_SESSION["admin_config"][mall_data_root]."/images/icon/".$idx.".gif");
+	}
+	//MO Icon
+	if ($_FILES["mo_icon_file"]["size"] > 0){
+		move_uploaded_file($_FILES["mo_icon_file"]["tmp_name"], $_SERVER["DOCUMENT_ROOT"]."".$_SESSION["admin_config"][mall_data_root]."/images/icon/mo_".$idx.".gif");
+		$db->query("update shop_icon set mo_idx = 'mo_".$idx."' where idx = '$idx' ");
 	}
 	echo "<script language='javascript' src='../js/message.js.php'></script><script>show_alert('아이콘이 등록 되었습니다..');parent.location.reload();</script>";
 }
@@ -24,8 +30,14 @@ if($act == "update"){
 	if(!is_dir($path)){
 		mkdir($path, 0777);
 	}
+	//PC Icon 
 	if ($_FILES["icon_file"]["size"] > 0){
 		move_uploaded_file($_FILES["icon_file"]["tmp_name"], $_SERVER["DOCUMENT_ROOT"]."".$_SESSION["admin_config"][mall_data_root]."/images/icon/".$idx.".gif");
+	}
+	//MO Icon
+	if ($_FILES["mo_icon_file"]["size"] > 0){
+		move_uploaded_file($_FILES["mo_icon_file"]["tmp_name"], $_SERVER["DOCUMENT_ROOT"]."".$_SESSION["admin_config"][mall_data_root]."/images/icon/mo_".$idx.".gif");
+		$db->query("update shop_icon set mo_idx = 'mo_".$idx."' where idx = '$idx' ");
 	}
 	echo "<script language='javascript' src='../js/message.js.php'></script><script>show_alert('아이콘이 수정 되었습니다..');parent.location.reload();</script>";
 }
